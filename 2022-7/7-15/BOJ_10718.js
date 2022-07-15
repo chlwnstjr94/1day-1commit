a0f4a120b454dadf47cfccff3da9a95e53396b58
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-07-16 03:56:04 +0900

    Add BOJ_10718.js

---
+++ 2022-7/7-15/BOJ_10718.js
@@ -0,0 +1,7 @@
+// readline 모듈보다는 fs를 이용해 파일 전체를 읽어 들여 처리하기
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+for (let i = 0; i < 2; i++) {
+  console.log('강한친구 대한육군')
+}
