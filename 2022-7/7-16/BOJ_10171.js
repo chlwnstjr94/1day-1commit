6cfedb5ec87a8c8e0f26b28a17904439ff55f10c
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-07-17 03:01:53 +0900

    add BOJ_10171.js

---
+++ 2022-7/7-16/BOJ_10171.js
@@ -0,0 +1,8 @@
+// readline 모듈보다는 fs를 이용해 파일 전체를 읽어 들여 처리하기
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+console.log('\\    /\\');
+console.log(' )  ( \')');
+console.log('(  /  )');
+console.log(' \\(__)|')
