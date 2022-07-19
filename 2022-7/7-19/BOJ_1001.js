b9e86cfce60aca00a0b765c049a0694f58e8620a
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-07-20 01:25:15 +0900

    add BOJ_1001.js

---
+++ 2022-7/7-19/BOJ_1001.js
@@ -0,0 +1,8 @@
+// readline 모듈보다는 fs를 이용해 파일 전체를 읽어 들여 처리하기
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let a = Number(input[0].split(' ')[0]);
+let b = Number(input[0].split(' ')[1]);
+
+console.log(a - b);
