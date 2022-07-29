bad3b0656d46e081943706203a135842c46658d5
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-07-29 16:45:47 +0900

    add BOJ_25083.js (2022-7-29(instead28))

---
+++ 2022-7/7-29(instead28)/BOJ_25083.js
@@ -0,0 +1,10 @@
+// readline 모듈보다는 fs를 이용해 파일 전체를 읽어 들여 처리하기
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+console.log("         ,r'\"7");
+console.log("r`-_   ,'  ,/");
+console.log(" \\. \". L_r'");
+console.log("   `~\\/");
+console.log("      |");
+console.log("      |");
