0e8204ac075a5f42ec738d675e216964633806d6
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-08-02 02:15:26 +0900

    add BOJ_14681.js (2022-8-1)

---
+++ 2022-8/8-1/BOJ_14681.js
@@ -0,0 +1,16 @@
+// readline 모듈보다는 fs를 이용해 파일 전체를 읽어 들여 처리하기
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let x = Number(input[0]);
+let y = Number(input[1]);
+
+if (x > 0 && y > 0) {
+  console.log(1);
+} else if (x < 0 && y > 0) {
+  console.log(2);
+} else if (x < 0 && y < 0) {
+  console.log(3);
+} else if (x > 0 && y < 0) {
+  console.log(4);
+}
