43fad358d8f5c33176490f8f066c10daf2fdfec0
chlwnstjr94 <chlwnstjr940404@gmail.com>
2023-03-06 22:00:39 +0900

    Add search bar (2023-3-6)

---
+++ 2023-3/3-6/autocomplete.js
@@ -0,0 +1,44 @@
+let availableKeywords = [
+  "HTML",
+  "CSS",
+  "Easy Tutorials",
+  "Web design tutorials",
+  "JavaScript",
+  "Where to learn coding online",
+  "Where to learn web design",
+  "How to create a website",
+];
+
+const resultBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function () {
+  let result = [];
+  let input = inputBox.value;
+
+  if (input.length) {
+    result = availableKeywords.filter((keyword) => {
+      return keyword.toLowerCase().includes(input.toLowerCase());
+    });
+    console.log(result);
+  }
+
+  display(result);
+
+  if (!result.length) {
+    resultBox.innerHTML = "";
+  }
+};
+
+function display(result) {
+  const content = result.map((list) => {
+    return "<li onclick=selectInput(this)>" + list + "</li>";
+  });
+
+  resultBox.innerHTML = "<ul>" + content.join("") + "</ul>";
+}
+
+function selectInput(list) {
+  inputBox.value = list.innerHTML;
+  resultBox.innerHTML = "";
+}
